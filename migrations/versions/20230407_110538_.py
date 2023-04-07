acbb9fa802774d7bd3ad5b8e48804f9b5bf71b8f
roysa <roysapeguero@outlook.com>
2023-04-07 11:12:28 -0700

    seed fix

--- migrations/versions/20230407_110538_.py
+++ migrations/versions/20230407_110538_.py
@@ -1,16 +1,19 @@
 """empty message
 
-Revision ID: 0a371a17ef02
-Revises: 
-Create Date: 2023-04-06 19:34:52.739117
+Revision ID: 7cf26e1dadbe
+Revises:
+Create Date: 2023-04-07 11:05:38.497819
 
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
-revision = '0a371a17ef02'
+revision = '7cf26e1dadbe'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -75,6 +78,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
